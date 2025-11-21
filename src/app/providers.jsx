95ba13fcf95c4702650f79642856fb849d4c6969
Divine Ugorji <divineugorji37@gmail.com>
2025-11-21 10:22:57 +0100

    Fix posthog analytics 🎉

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -10,10 +10,17 @@ import { PostHogProvider as PHProvider } from "posthog-js/react";
 
 export function PostHogProvider({ children }) {
   useEffect(() => {
+    // posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+    //   api_host:
+    //     process.env.NEXT_PUBLIC_POSTHOG_KEY || "https://us.i.posthog.com",
+    //   person_profiles: "always", // or 'always' to create profiles for anonymous users as well
+    //   capture_pageview: true,
+    // });
+
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host:
-        process.env.NEXT_PUBLIC_POSTHOG_KEY || "https://us.i.posthog.com",
-      person_profiles: "always", // or 'always' to create profiles for anonymous users as well
+        process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
+      person_profiles: "always",
       capture_pageview: true,
     });
   }, []);

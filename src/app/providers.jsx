eb7bfad2413a5f52cb98a564a264a5e963e7de6b
Divine Ugorji <divineugorji37@gmail.com>
2025-11-21 10:05:34 +0100

    Fix posthog analytics 🎉

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -13,8 +13,7 @@ export function PostHogProvider({ children }) {
     posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
       api_host:
         process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://us.i.posthog.com",
-      person_profiles: "identified_only", // or 'always' to create profiles for anonymous users as well
-      //   defaults: "2025-05-24",
+      person_profiles: "always", // or 'always' to create profiles for anonymous users as well
       capture_pageview: true,
     });
   }, []);
